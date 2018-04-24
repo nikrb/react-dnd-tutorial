8d878c96c1b02121ba7595ea4eea615cdf6450eb
nikrb <nik_rb@yahoo.com>
2018-04-24 15:08:32 +0100

    finish drop target color highlighting

--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -6,6 +6,10 @@ import { ItemTypes } from './constants';
 import { DropTarget } from 'react-dnd';
 
 const squareTarget = {
+  canDrop(props) {
+    return canMoveKnight(props.x, props.y);
+  },
+
   drop(props, monitor) {
     // FIXME: monitor.getItem to receive dragged item
     moveKnight(props.x, props.y);
@@ -16,6 +20,7 @@ function collect(connect, monitor) {
   return {
     connectDropTarget: connect.dropTarget(),
     isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
   };
 }
 
@@ -25,12 +30,26 @@ class BoardSquare extends React.Component {
     y: PropTypes.number.isRequired,
     connectDropTarget: PropTypes.func.isRequired,
     isOver: PropTypes.bool.isRequired,
+    canDrop: PropTypes.bool.isRequired,
   };
-  render() {
-    const { x, y, connectDropTarget, isOver } = this.props;
-    console.log('isOver:', isOver);
-    const black = (x + y) % 2 === 1;
+  renderOverlay(color) {
     return (
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        opacity: 0.5,
+        backgroundColor: color,
+      }} />
+    );
+  }
+  render() {
+    const { x, y, connectDropTarget, isOver, canDrop } = this.props;
+    const black = (x + y) % 2 === 1;
+    return connectDropTarget(
       <div style={{
           position: 'relative',
           width: '100%',
@@ -39,18 +58,9 @@ class BoardSquare extends React.Component {
         <Square black={black} >
           {this.props.children}
         </Square>
-        {isOver &&
-          <div style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              height: '100%',
-              width: '100%',
-              zIndex: 1,
-              opacity: 0.5,
-              backgroundColor: 'yellow',
-          }} />
-        }
+        {isOver && !canDrop && this.renderOverlay('red')}
+        {!isOver && canDrop && this.renderOverlay('yellow')}
+        {isOver && canDrop && this.renderOverlay('green')}
       </div>
     );
   }
